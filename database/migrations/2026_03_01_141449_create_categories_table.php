47c083882b0d8448a721da006f0ec53ad49422c8
ahasan0015 <ahasanstu@gmail.com>
2026-03-08 03:13:50 +0600

    delete brand_id schema from categories table

--- database/migrations/2026_03_01_141449_create_categories_table.php
+++ database/migrations/2026_03_01_141449_create_categories_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('brand_id')->nullable();
             $table->string('name');
             $table->string('slug')->unique();
             $table->unsignedBigInteger('status_id')->nullable();
